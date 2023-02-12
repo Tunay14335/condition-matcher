3b1cbdbbdd9d49a3a6e30d25f21abcf5e80fcd3a
Tunay14335 <68521569+Tunay14335@users.noreply.github.com>
2023-02-12 19:57:32 +0300

    First Commit

---
+++ ConditionMatcher/BitInstructor.cs
@@ -0,0 +1,34 @@
+
+namespace ConditionMatch
+{
+    public class BitInstructor : IEquatable<BitInstructor>
+    {
+        public BitInstructor(params bool[] bitArray)
+        {
+            BitCount = bitArray.Count<bool>();
+            if(BitCount > 32)
+            {
+                throw new Exception("`bitArray` length must be lower than 32");
+            }
+            m_value = 0;
+            m_value |= bitArray[0] ? 1 : 0;
+            for(int i = 1 ; i < bitArray.Length ; i++)
+            {
+                m_value <<= 1;
+                m_value |= bitArray[i] ? 1 : 0;
+            }
+        }
+        public bool Equals(BitInstructor? obj)
+        {
+            return obj != null && obj.Value == this.Value;
+        }
+
+        public override string ToString() => m_value.ToString();
+
+        public int BitCount {get; private set;}
+
+        public int Value => m_value;
+        private int m_value;
+
+    }
+}
